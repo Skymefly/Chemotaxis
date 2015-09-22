575f1576424775122c0a38e1b642b5c75ed91c1f
unknown <sktang@697-334-Erebor.sf.k12.internal>
2015-09-22 11:33:59 -0700

    Added some stuff but stuck with dots

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,11 +1,27 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
  //declare bacteria variables here
  int [] Bacteria;
  Bacteria [] colony;
  int x;
  int y;
  boolean alive;
- color BacteriaColor = color(245, 147, 168);   
- void setup()   
+ int BacteriaColor = color(245, 147, 168);   
+ public void setup()   
  {     
  	//initialize bacteria variables here
  	background(0);
@@ -14,7 +30,7 @@
  	alive=true;
 
  }   
- void draw()   
+ public void draw()   
  {    
  	//move and show the bacteria
  	for (int i=0; i<colony.length; i++)
@@ -33,7 +49,7 @@
  		myX=x;
  		myY=y;
  	}
- 	void show()
+ 	public void show()
  	{
  		if (get(myX,myY)!=color(0))
  		{
@@ -46,10 +62,19 @@
 		 	ellipse(myX, myY, 10, 10);
 	 	}
  	}
- 	void move()
+ 	public void move()
  	{
 	 		myX=myX+((int)(Math.random()*10));
 	 		myY=myY+((int)(Math.random()*10));
  	}
 
  }    
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
